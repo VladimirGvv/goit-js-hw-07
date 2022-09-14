66b56ed28f324fb98a6fb3db4115634aeb67afd7
VladimirGvv <vovagvv69@gmail.com>
2022-09-14 14:21:19 +0300

    fix lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -17,9 +17,9 @@ function createMarckup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-   <a class="gallery__item" href="${preview}">
+   <a class="gallery__item" href="${original}">
         <img class="gallery__image" 
-        src="${original}" 
+        src="${preview}" 
         alt="${description}" />
     </a>
     `;
